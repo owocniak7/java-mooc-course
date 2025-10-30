30936680bcc912ceb911cfa935f878b4036f9577
owocniak7 <antoni.wawrzyniak1@gmail.com>
2025-10-30 16:53:54 +0100

    [ADD] Part04_18.PersonalInformation

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,26 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            System.out.println("First name: ");
+            String firstName = scanner.nextLine();
+            
+            if (firstName.isEmpty()) {
+                break;
+            }
+            
+            System.out.println("Last name: ");
+            String lastName = scanner.nextLine();
+            
+            System.out.println("Identification number: ");
+            String identificationNumber = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, identificationNumber));
+        }
+        
+        for (PersonalInformation information : infoCollection) {
+            System.out.println(information.getFirstName() + " " + information.getLastName());
+        }
     }
 }
